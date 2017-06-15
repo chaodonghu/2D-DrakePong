925ccf23833778f977a25a44af1a9df7e9fd72c4
Dong Hu <cdhu@uwaterloo.ca>
2017-06-15 16:51:55 -0400

    Add working game

---
+++ main.js
@@ -0,0 +1,22 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
+
+var spacefield;
+
+var mainState = {
+  preload:function(){
+
+  },
+
+  create:function(){
+
+  },
+
+  update:function(){
+
+  }
+
+}
+
+game.state.add('mainState', mainState);
+
+game.state.start("mainstate");
